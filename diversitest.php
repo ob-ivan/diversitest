b0d10a2e8c89b8806f393df4989aeb716f1e1bc2
Ob-Ivan <ob-ivan@users.noreply.github.com>
2018-02-13 00:52:05 +0300

    Add autoloader discovery

--- diversitest.php
+++ diversitest.php
@@ -1,5 +1,19 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+if (!(function () {
+    $dirname = __DIR__;
+    for ($i = 0; $i < 10; ++$i) {
+        $autoload = "$dirname/vendor/autoload.php";
+        if (file_exists($autoload)) {
+            require_once $autoload;
+            return true;
+        }
+        $dirname = dirname($dirname);
+    }
+    return false;
+})()) {
+    print "Could not find autoloader for " . __DIR__ . "\n";
+    exit(1);
+}
 
 use Ob_Ivan\DiversiTest\DiversiTestCommand;
 use Symfony\Component\Console\Application;
