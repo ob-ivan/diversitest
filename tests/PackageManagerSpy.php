5785896bd5149b5f19e51ae9cf67164274917553
Ob-Ivan <ob-ivan@users.noreply.github.com>
2018-02-24 01:53:42 +0300

    Add factory test stub

---
+++ tests/PackageManagerSpy.php
@@ -0,0 +1,8 @@
+<?php
+namespace tests;
+
+use Ob_Ivan\DiversiTest\PackageManager;
+
+class PackageManagerSpy extends PackageManager
+{
+}
--- tests/PackageManagerTest.php
+++ tests/PackageManagerTest.php
@@ -6,6 +6,45 @@ use PHPUnit\Framework\TestCase;
 
 class PackageManagerTest extends TestCase
 {
+    /**
+     * @dataProvider provideFromConfig
+     */
+    public function testFromConfig(
+        $config,
+        string $expectedCommandLine,
+        $expectedTemplateEngine,
+        $expectedIterationType
+    ) {
+        $packageManager = PackageManagerSpy::fromConfig($config);
+        $this->assertEquals(
+            $expectedCommandLine,
+            $packageManager->getCommandLine(),
+            'Command line MUST match'
+        );
+        $this->assertEquals(
+            $expectedTemplateEngine,
+            $packageManager->getTemplateEngine(),
+            'Template engine MUST match'
+        );
+        $this->assertEquals(
+            $expectedIterationType,
+            $packageManager->getIterationType(),
+            'Iteration type MUST match'
+        );
+    }
+
+    public function provideFromConfig()
+    {
+        return [
+            [
+                'config' => 'echo $package $version',
+                'expectedCommandLine' => 'echo $package $version',
+                'expectedTemplateEngine' => PackageManager::TEMPLATE_SHELL,
+                'expectedIterationType' => PackageManager::ITERATE_PACKAGE,
+            ],
+        ];
+    }
+
     public function testGetCommands()
     {
         $packageManager = new PackageManager(
