d375e9b8ab9c2d74db4b5b94f6733d892b540bc0
ob-ivan <ob-ivan@users.noreply.github.com>
2021-05-27 21:13:42 +0200

    Handle exceptions in 'diversitest.php'

--- diversitest.php
+++ diversitest.php
@@ -23,8 +23,18 @@ require_once $autoload;
 use Ob_Ivan\DiversiTest\DiversiTestCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application();
-$command = new DiversiTestCommand($config);
-$application->add($command);
-$application->setDefaultCommand($command->getName(), true);
-$application->run();
+try {
+    $application = new Application();
+    $command = new DiversiTestCommand($config);
+    $application->add($command);
+    $application->setDefaultCommand($command->getName(), true);
+    $application->run();
+}
+catch (Exception $e) {
+    do {
+        echo 'Exception ' . get_class($e) . ' (' . $e->getCode() . '): "' . $e->getMessage() . '"' . PHP_EOL;
+        echo 'Thrown in ' . $e->getFile() . ':' . $e->getLine() . PHP_EOL;
+        echo 'Stack trace: ' . $e->getTraceAsString() . PHP_EOL;
+    }
+    while ($e = $e->getPrevious());
+}
