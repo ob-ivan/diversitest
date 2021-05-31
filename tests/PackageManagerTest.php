5874645b66d85d4af4aecce3c5b6bf0dacb8635e
ob-ivan <ob-ivan@users.noreply.github.com>
2021-05-31 15:41:19 +0200

    Move the factory tests to the new test class

--- tests/PackageManagerTest.php
+++ tests/PackageManagerTest.php
@@ -8,67 +8,6 @@ use PHPUnit\Framework\TestCase;
 class PackageManagerTest extends TestCase
 {
     /**
-     * @param array|string $config
-     * @param string $expectedCommandLine
-     * @param string expectedTemplateEngine
-     * @param string $expectedIterationType
-     * @throws InvalidConfigException
-     * @dataProvider provider_fromConfig
-     */
-    public function test_fromConfig(
-        $config,
-        $expectedCommandLine,
-        $expectedTemplateEngine,
-        $expectedIterationType
-    ) {
-        $packageManager = PackageManagerSpy::fromConfig($config);
-        $this->assertEquals(
-            $expectedCommandLine,
-            $packageManager->getCommandLine(),
-            'Command line MUST match'
-        );
-        $this->assertEquals(
-            $expectedTemplateEngine,
-            $packageManager->getTemplateEngine(),
-            'Template engine MUST match'
-        );
-        $this->assertEquals(
-            $expectedIterationType,
-            $packageManager->getIterationType(),
-            'Iteration type MUST match'
-        );
-    }
-
-    public function provider_fromConfig()
-    {
-        return [
-            [
-                'config' => 'echo $package $version',
-                'expectedCommandLine' => 'echo $package $version',
-                'expectedTemplateEngine' => PackageManager::TEMPLATE_SHELL,
-                'expectedIterationType' => PackageManager::ITERATE_PACKAGE,
-            ],
-            [
-                'config' => [
-                    'command_line' => 'hello world',
-                    'template_engine' => 'twig',
-                    'iteration_type' => 'configuration'
-                ],
-                'expectedCommandLine' => 'hello world',
-                'expectedTemplateEngine' => PackageManager::TEMPLATE_TWIG,
-                'expectedIterationType' => PackageManager::ITERATE_CONFIGURATION,
-            ],
-            [
-                'config' => 'composer',
-                'expectedCommandLine' => 'composer require {% for p, v in configuration %}{{ p }}:{{ v }} {% endfor %}',
-                'expectedTemplateEngine' => PackageManager::TEMPLATE_TWIG,
-                'expectedIterationType' => PackageManager::ITERATE_CONFIGURATION,
-            ],
-        ];
-    }
-
-
-    /**
      * @param PackageManager $packageManager
      * @param array $configuration
      * @param array $expectedCommands
