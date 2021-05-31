0972fe531b6e3d915454452ee0b182001993a8be
ob-ivan <ob-ivan@users.noreply.github.com>
2021-05-31 16:09:09 +0200

    Create a package manager config class

---
+++ src/PackageManagerConfig.php
@@ -0,0 +1,39 @@
+<?php
+namespace Ob_Ivan\DiversiTest;
+
+class PackageManagerConfig
+{
+    protected $commandLine;
+    protected $templateEngine;
+    protected $iterationType;
+
+    /**
+     * @param string $commandLine
+     * @param string $templateEngine
+     * @param string $iterationType
+     */
+    public function __construct($commandLine, $templateEngine, $iterationType)
+    {
+        $this->commandLine = $commandLine;
+        $this->templateEngine = $templateEngine;
+        $this->iterationType = $iterationType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCommandLine()
+    {
+        return $this->commandLine;
+    }
+
+    public function getTemplateEngine()
+    {
+        return $this->templateEngine;
+    }
+
+    public function getIterationType()
+    {
+        return $this->iterationType;
+    }
+}
